17eefe47bb48b1a20806448c76dc4ba67225fcac
BradHamm <bradham.opt@gmail.com>
2023-05-16 20:05:43 -0700

    Functionality for Favorite Spots is now working! Save button on homepage saves information locally.

--- fav-spots/fav.spots.js
+++ fav-spots/fav.spots.js
@@ -17,20 +17,26 @@ document.addEventListener('DOMContentLoaded', function () {
   function appendBreweryCard(data) {
     console.log(data);
     var container = document.getElementById('favoriteBreweriesList');
+
+    var outerCardDiv = document.createElement('div');
+    outerCardDiv.classList.add('tile' , 'is-vertical' ,  'is-12' ,  'is-warning' ,  'is-parent');
   
-    var cardDiv = document.createElement('div');
-    cardDiv.classList.add('tile', 'is-vertical', 'is-12', 'is-warning', 'is-parent');
+    var cardDiv = document.createElement('section');
+    cardDiv.classList.add('is-child' ,  'notification' ,  'is-warning');
   
     // Create and append elements for brewery name, type, and address
-    var breweryName = document.createElement('h2');
+    var breweryName = document.createElement('p');
+    breweryName.classList.add('title');
     breweryName.textContent = data.name;
     cardDiv.appendChild(breweryName);
   
     var breweryType = document.createElement('p');
+    breweryType.classList.add('subtitle');
     breweryType.textContent = data.type;
     cardDiv.appendChild(breweryType);
   
     var breweryAddress = document.createElement('p');
+    breweryAddress.classList.add('subtitle');
     breweryAddress.textContent = data.address;
     cardDiv.appendChild(breweryAddress);
   
