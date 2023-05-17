b4dd5f9397c42a30468038bacc15b3e1e67ac0ec
BradHamm <bradham.opt@gmail.com>
2023-05-16 21:05:01 -0700

    Finalizing functionality of the favorite spots page. Need to add functionality to OPDB Address and Type

--- fav-spots/fav.spots.js
+++ fav-spots/fav.spots.js
@@ -1,3 +1,58 @@
+function searchAndSaveBreweries() {
+    var searchInput = document.getElementById('searchBrew').value;
+  
+    var apiUrl =
+      'https://api.openbrewerydb.org/v1/breweries/autocomplete?query=' +
+      searchInput;
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data.length > 0) {
+          var breweryId = data[0].id;
+          specifyBreweries(breweryId, searchInput)
+            .then(function (additionalData) {
+              saveBreweryLocally(additionalData); // Save the brewery locally
+              renderSavedBreweries(); // Update the display
+            })
+            .catch(function (error) {
+              console.log('Error:', error);
+            });
+        } else {
+          console.log('No breweries found for the search input:', searchInput);
+        }
+      })
+      .catch(function (error) {
+        console.log('Error:', error);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    renderSavedBreweries();
+  });
+  
+  function saveBreweryLocally(breweryInfo) {
+    var savedBreweries = localStorage.getItem('savedBreweries');
+    var breweries = savedBreweries ? JSON.parse(savedBreweries) : [];
+    breweries.push(breweryInfo);
+    localStorage.setItem('savedBreweries', JSON.stringify(breweries));
+  }
+  
+  function renderSavedBreweries() {
+    var container = document.getElementById('favoriteBreweriesList');
+    container.innerHTML = '';
+  
+    var savedBreweries = localStorage.getItem('savedBreweries');
+    if (savedBreweries) {
+      var breweries = JSON.parse(savedBreweries);
+      breweries.forEach(function (breweryInfo) {
+        appendBreweryCard(breweryInfo);
+      });
+    }
+  }  
+
 document.addEventListener('DOMContentLoaded', function () {
     renderSavedBreweries();
   });
@@ -39,7 +94,38 @@ document.addEventListener('DOMContentLoaded', function () {
     breweryAddress.classList.add('subtitle');
     breweryAddress.textContent = data.address;
     cardDiv.appendChild(breweryAddress);
+
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+    deleteBreweryCard(cardDiv, data);
+    });
+    cardDiv.appendChild(deleteButton);
   
     // Append the card to the container
     container.appendChild(cardDiv);
   }
+
+  function deleteBreweryCard(cardDiv, savedBrewery) {
+    var container = document.getElementById('favoriteBreweriesList');
+    
+    // Remove the card from the container
+    container.removeChild(cardDiv);
+  
+    // Remove the saved brewery from local storage
+    var savedBreweries = localStorage.getItem('savedBreweries');
+    if (savedBreweries) {
+      var breweries = JSON.parse(savedBreweries);
+      var index = breweries.findIndex(function (brewery) {
+        return brewery.name === savedBrewery.name && brewery.type === savedBrewery.type && brewery.address === savedBrewery.address;
+      });
+      if (index !== -1) {
+        breweries.splice(index, 1);
+        localStorage.setItem('savedBreweries', JSON.stringify(breweries));
+      }
+    }
+  }
+
+  document.getElementById('saveToList').addEventListener('click', function () {
+    searchAndSaveBreweries();
+  });
